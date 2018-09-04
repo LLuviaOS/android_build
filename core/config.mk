AOSiP/platform_build

build: config.mk: use our updated flex if FLEX_EXEC isn't set
Change-Id: Ic7fde5d6a1c28ab3cf4b62f75cd06c15448ff5cf
Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

core/config.mk
@@ -585,7 +585,7 @@ JACK := $(HOST_OUT_EXECUTABLES)/jack
 ifneq ($(FLEX_EXEC),)
 LEX := $(FLEX_EXEC)
 else
-LEX := prebuilts/misc/$(BUILD_OS)-$(HOST_PREBUILT_ARCH)/flex/flex-2.5.39
+LEX := vendor/aosip/prebuilt/flex/flex-2.6.4
 endif
 # The default PKGDATADIR built in the prebuilt bison is a relative path
 # external/bison/data.

LLuviaOS/android_build

Signed-off-by: Ram9199 <sriramscience7@gmail.com>

core/config.mk
@@ -585,7 +585,7 @@ JACK := $(HOST_OUT_EXECUTABLES)/jack
 ifneq ($(FLEX_EXEC),)
 LEX := $(FLEX_EXEC)
 else
-LEX := prebuilts/misc/$(BUILD_OS)-$(HOST_PREBUILT_ARCH)/flex/flex-2.5.39
+LEX := vendor/lluvia/prebuilt/flex/flex-2.6.4
 endif
 # The default PKGDATADIR built in the prebuilt bison is a relative path
 # external/bison/data.